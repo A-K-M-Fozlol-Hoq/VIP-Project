32a1bc91c9005579bfe4b71e2492c85172e3efd7
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2021-10-31 21:49:56 +0600

    login page done

--- Login/loginScript.js
+++ Login/loginScript.js
@@ -1,11 +1,36 @@
 const loginLeftPart = document.getElementById('login-left-part')
-// const loginRightPart = document.getElementById('login-right-part')
 const loginSignupForm = document.getElementById('login-signup-form')
 const loginCreateAccount = document.getElementById('login-create-account')
+const loginForm = document.getElementById('login-form')
+
+const loginRightPart = document.getElementById('login-right-part')
+const loginInstead = document.getElementById('login-instead')
+
+
 loginCreateAccount.addEventListener('click', (event) => {
     event.preventDefault();
-    console.log(event)
-    loginLeftPart.style.visibility = 'hidden'
-    // loginLeftPart.style.display = 'none'
-    loginSignupForm.style.display = 'block'
+    console.log(event.target);
+    loginSignupForm.style.display = 'block';
+    loginRightPart.style.background = 'none';
+
+    loginLeftPart.style.backgroundImage = "url('./images/loginBG\ \(1\).jpg')";
+    loginLeftPart.style.backgroundSize = "700px 100%";
+
+    loginForm.style.visibility = "hidden"
+
+});
+
+loginInstead.addEventListener('click', (event) =>{
+    event.preventDefault();
+    console.log(event.target);
+
+    loginSignupForm.style.display = 'none';
+    loginRightPart.style.backgroundImage = "url('./images/loginBG\ \(2\).jpg')";
+    loginRightPart.style.backgroundSize = "700px 100%";
+
+    loginLeftPart.style.background = 'none';
+
+    loginForm.style.visibility = "visible"
+
+
 })
