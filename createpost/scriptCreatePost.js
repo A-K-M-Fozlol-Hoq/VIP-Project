68817a894912188b79960e3a23af6f55a2d3d0da
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-11-21 23:05:39 +0600

    fixed image upload bug

--- createpost/scriptCreatePost.js
+++ createpost/scriptCreatePost.js
@@ -1,4 +1,3 @@
-
 const addTextBtn = document.getElementById('add-text-btn')
 const postTextInput = document.getElementById('post-text-input')
 const postTextDiv = document.getElementById('post-text-div')
@@ -23,27 +22,36 @@ felling.addEventListener('change', (event) => {
 });
 
  postImageInput.addEventListener ('change',async(event) => {
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add('post-image-div')
     const img = document.createElement("img");
-    const liveImg = await uploadImage(event.target.files[0])
+    const crossBtnDiv = document.createElement("div");
+    crossBtnDiv.classList.add('img-cross-div')
+    const crossBtn = document.createElement("p");
+    crossBtn.innerText='x';
+    let imgSrc= URL.createObjectURL(event.target.files[0]);
+    img.src = imgSrc;
+    imgDiv.appendChild(img)
+    crossBtnDiv.appendChild(crossBtn)
+    imgDiv.appendChild(crossBtnDiv)
+    postImagesDiv.appendChild(imgDiv)
+    // const liveImg = await uploadImage(event.target.files[0])
     // img.src = liveImg;
-    // console.log(img);
-    // postImagesDiv.appendChild(img)
+    console.log(postImagesDiv);
 })
+
 async function uploadImage(img)
  {
-     console.log(img);
+    console.log(img);
     var form = new FormData();
     form.append('image', img)
-    var url = 'https://api.imgbb.com/1/upload?key=b33c215182400d63985c1c33c5ecf50d' 
+    var url = 'https://api.imgbb.com/1/upload?key=b33c215182400d63985c1c33c5ecf50d';
     const config = {
         method: 'POST',
         mode: 'no-cors',
         headers: {
-            'Accept': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            "Access-Control-Allow-Credentials" : true ,
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
         },
         body: form,
     }
@@ -51,4 +59,20 @@ async function uploadImage(img)
     // const json = await response.json()
     console.log(response)
     // return response.url;
+
+    // const profileData = new FormData();
+    // profileData.set("key", "b33c215182400d63985c1c33c5ecf50d");
+    // profileData.append("image", img);
+
+    // fetch("https://api.imgbb.com/1/upload", {
+    //       method: "POST",
+    //       profileData
+    //     })
+    //       .then((response) => response.json())
+    //       .then((data) => {
+    //         console.log(data);
+    //       })
+    //       .catch((error) => {
+    //         console.error(error);
+    //       });
  }
