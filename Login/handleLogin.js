15d80938592eef9bf2c9a3680213ef9a18e930de
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-11-29 10:33:28 +0600

    fixed login error, work on progress

--- Login/handleLogin.js
+++ Login/handleLogin.js
@@ -1,6 +1,8 @@
 const loginCreateAccountID = document.getElementById('login-create-account');
 const loginInsteadID = document.getElementById('login-instead');
 const loginEmailID = document.getElementById('login-email');
+const loginBtnID = document.getElementById('login-btn');
+const createAccountBtnID = document.getElementById('create-account-btn');
 let isLogging = true;
 const loginObject ={
     email:'',
@@ -24,6 +26,21 @@ loginInsteadID.addEventListener('click', () => {
     console.log('loginInsteadID',isLogging);
 })
 
+//update login data
 function changeLoginInfo(event){
-    console.log(event.target.value);
+    console.log(event.target.name, event.target.value);
 }
+//update signup data
+function changeSignupInfo(event){
+    console.log(event.target.name, event.target.value);
+}
+
+//handle login button
+loginBtnID.addEventListener('click', ()=>{
+    console.log('loginBtnID');
+})
+
+//handle signup button
+createAccountBtnID.addEventListener('click', ()=>{
+    console.log('createAccountBtnID');
+})
