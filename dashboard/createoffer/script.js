472d605fcacc2107f8379fbe0499dca80036c1e7
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-12-02 17:14:25 +0600

    create offer on progress

--- dashboard/createoffer/script.js
+++ dashboard/createoffer/script.js
@@ -1,11 +1,21 @@
 
 const gigTitleInput = document.getElementById('gig-title-input')
 const gigSubTitleInput = document.getElementById('gig-sub-title-input')
+const gigTagInput = document.getElementById('gig-tag-input')
+const addGigTagBtn = document.getElementById('add-gig-tag-btn')
+let totalTag=0;
 
-function changeTitle(event) {
+function changeTitle() {
     console.log(gigTitleInput.name, gigTitleInput.value);
 }
 
-function changeSubTitle(event) {
+function changeSubTitle() {
     console.log(gigTitleInput.name, gigSubTitleInput.value);
 }
+
+// function addTag() {
+//     console.log(gigTagInput.name, gigTagInput.value);
+// }
+addGigTagBtn.addEventListener("click", ()=>{
+    console.log('object', gigTagInput.value);
+})
